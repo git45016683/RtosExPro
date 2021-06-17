70780c0c835cef7fab2e49af307c35b544416f89
icemaple <450196683@qq.com>
2021-06-17 09:36:36 +0800

    cubeMX工程开启串口接收DMA

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RtosExPro
 This is a repository for rtos sample project.
+
+# reference
+[STM32 use FreeRTOS CLI](https://www.programmersought.com/article/84803197029/)
+[STM32使用FreeRTOS CLI](https://blog.csdn.net/qq_41544116/article/details/105095063)
+
--- rtosex/Core/Src/main.c
+++ rtosex/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -144,6 +145,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
